3bae8f45270c93bb529faa2405e7a66f548b98f8
Larry Mitchell <118214035+lmitchell137@users.noreply.github.com>
2026-03-10 16:48:45 -0400

    chore: tighten sky vulkan module exports

--- dynamissky-vulkan/src/main/java/module-info.java
+++ dynamissky-vulkan/src/main/java/module-info.java
@@ -7,11 +7,7 @@ module org.dynamissky.vulkan {
     requires org.lwjgl.vulkan;
 
     exports org.dynamissky.vulkan;
-    exports org.dynamissky.vulkan.descriptor;
     exports org.dynamissky.vulkan.lut;
-    exports org.dynamissky.vulkan.pass;
     exports org.dynamissky.vulkan.stars;
-    exports org.dynamissky.vulkan.moon;
-    exports org.dynamissky.vulkan.hdri;
     exports org.dynamissky.vulkan.integration;
 }
